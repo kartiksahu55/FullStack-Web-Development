2c709a8feda4dff76c977ce592e13c9ac8da2463
Kartik Chandra Sahu <125174136+kartiksahu55@users.noreply.github.com>
2023-03-26 00:00:52 +0530

    Update 5. min&MaxValues.js

--- Destructuring Spread &Rest/5. min&MaxValues.js
+++ Destructuring Spread &Rest/5. min&MaxValues.js
@@ -1,11 +1,9 @@
 const numbers = [5, 2, 7, 1, 9];
-function minMax(numbers) {
 
+function minMax(numbers) {
     const max = Math.max(...numbers);
     const min = Math.min(...numbers);
     return {max, min}
 }
-
-
 
 console.log(minMax(numbers));
