3b939b6f0032ae13fd0962fa4090b6fe7ece4073
Kartik Chandra Sahu <125174136+kartiksahu55@users.noreply.github.com>
2023-03-26 00:02:51 +0530

    Update 3. swapValues.js

--- Destructuring Spread &Rest/3. swapValues.js
+++ Destructuring Spread &Rest/3. swapValues.js
@@ -2,8 +2,8 @@ const x = 5;
 const y = 10;
 
 function swapValue(x, y) {
-    [ x, y]  = [ y, x] ;
-    return [x,y]
-}
+    [ x, y]  = [ y, x];
+    return [x,y];
+};
 
 console.log(swapValue(x, y));
