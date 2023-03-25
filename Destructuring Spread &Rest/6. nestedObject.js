07c9c904abfc996d4ea203cf43557a17bfbbde06
Kartik Chandra Sahu <125174136+kartiksahu55@users.noreply.github.com>
2023-03-26 00:04:35 +0530

    Update 6. nestedObject.js

--- Destructuring Spread &Rest/6. nestedObject.js
+++ Destructuring Spread &Rest/6. nestedObject.js
@@ -6,15 +6,15 @@ const person = {
         city: "Sector 62, Noida",
         state: "Uttar Pradesh"
     },
-}
+};
 
 function nested(person) {
     const { name, address: { street } } = person;
     return { name, street };
 
-    //    const name= person.name
-    //    const street=person.address.street
-    //     return {name, street}
+    //    const name= person.name;
+    //    const street=person.address.street;
+    //    return {name, street};
 };
 
 console.log(nested(person));
